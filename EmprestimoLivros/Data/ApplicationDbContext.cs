87485bc27efdf16a9c3a7528f1cfe0e68eb9372c
rafaa <rafaa@DESKTOP-L76F3L2>
2026-07-06 16:54:20 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<EmprestimoModel> Emprestimos { get; set; }
+    }
+}
